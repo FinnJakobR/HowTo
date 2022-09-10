cde4a38d664d0c0f55816612ada3d1d61089a133
Finn Reinhardt <65501534+FinnJakobR@users.noreply.github.com>
2022-09-10 19:53:01 +0200

    Delete UnixTests directory

--- UnixTests/SocketTests.js
+++
@@ -1,9 +0,0 @@
-const SocketClient = require("socket.io-client");
-
-const socket = SocketClient("http://localhost:3000");
-
-function TestsSocketEventListenerClass(){
- socket.emit("TEST"); 
-}
-
-TestsSocketEventListenerClass();
